8378fcf3157436d01be210b90c44c4081b172336
Szkaradd <szkaradekmikolaj@gmail.com>
2023-07-28 11:44:06 +0200

    solved whooole exercise

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { WatchComponent } from './watch/watch.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', 
+    component: HomeComponent
+  },
+  { path: 'about-me',
+    component: AboutMeComponent
+  },
+  { path: 'contact',
+    component: ContactComponent
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: 'watch/:id',
+    component: WatchComponent
+  },
+  {
+    path: 'watch',
+    redirectTo: '/watch/0',
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
